2a8076e8ca199ac9c30c634876c6f530f1797ba4
pavani286 <vpavani.v@gmail.com>
2022-06-30 14:16:37 -0500

    Samplepage & Sampletest classes  commit

---
+++ src/main/java/com/mytrip/qa/pages/samplepage.java
@@ -0,0 +1,8 @@
+package com.mytrip.qa.pages;
+
+public class samplepage {
+
+	public void test() {
+		System.out.println("test method ....");
+	}
+}
---
+++ src/test/java/com/mytrip/qa/testcases/sampletest.java
@@ -0,0 +1,8 @@
+package com.mytrip.qa.testcases;
+
+public class sampletest {
+
+	public void testcase() {
+		System.out.println("logic of tesctcase returned here ...");
+	}
+}
